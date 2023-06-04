888cdfb9ab14b538fe6ce7371333675f701963a8
kro0ol <clics_98@mail.ru>
2023-06-04 22:42:54 +0700

    adding an id property to the model class

--- Models/NvidiaGraphicsCardsGF.cs
+++ Models/NvidiaGraphicsCardsGF.cs
@@ -2,6 +2,7 @@
 {
     public class NvidiaGraphicsCardsGF
     {
+        public Guid Id { get; set; } = Guid.Empty;
         public string Series { get; set; }
         public string Memory { get; set; }
         public int Bits { get; set; }
