3905e8992fb277541285d6b15b87805a7fe6d66d
kro0ol <clics_98@mail.ru>
2023-06-04 22:31:10 +0700

    adding functionality Version Controller

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using admLab1.Models;
+
+namespace admLab1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var versionInfo = new Versions
+            {
+                Company = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company,
+                Product = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product,
+                ProductVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+            };
+
+            return Ok(versionInfo);
+        }
+    }
+}
---
+++ Models/Versions.cs
@@ -0,0 +1,9 @@
+﻿namespace admLab1.Models
+{
+    public class Versions
+    {
+        public string Company { get; set; }
+        public string ProductVersion { get; set; }
+        public string Product { get; set; }
+    }
+}
